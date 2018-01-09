4a93d72e99460e865f94a5b71a19fe98dd7dd85b
Gyumeijie <Gyumeijie@gmail.com>
2018-01-09 16:41:34 +0800

    add TestCaseDummyTracer_1 unit testcase for DC_DummyTracer.

--- src/RegressionTest/RegressionTestTypeRegister.c
+++ src/RegressionTest/RegressionTestTypeRegister.c
@@ -2,10 +2,12 @@
 #include "TestCaseRootObject_3.h"
 #include "TestCaseRootObject_2.h"
 #include "TestCaseRootObject_1.h"
+#include "TestCaseDummyTracer_1.h"
 
 void regression_test_type_register()
 {
    TestCaseRootObject_3_register();
    TestCaseRootObject_2_register();
    TestCaseRootObject_1_register();
+   TestCaseDummyTracer_1_register(); 
 }
