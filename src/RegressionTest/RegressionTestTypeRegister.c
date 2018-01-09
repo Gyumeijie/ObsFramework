d01c4fc549edf9b9083e0ed06e8a3ef6e5b005de
Gyumeijie <Gyumeijie@gmail.com>
2018-01-09 16:45:08 +0800

    add three unit testcases for DC_EventRepository.

--- src/RegressionTest/RegressionTestTypeRegister.c
+++ src/RegressionTest/RegressionTestTypeRegister.c
@@ -3,11 +3,19 @@
 #include "TestCaseRootObject_2.h"
 #include "TestCaseRootObject_1.h"
 #include "TestCaseDummyTracer_1.h"
+#include "TestCaseEventRepository_1.h"
+#include "TestCaseEventRepository_2.h"
+#include "TestCaseEventRepository_3.h"
 
 void regression_test_type_register()
 {
    TestCaseRootObject_3_register();
    TestCaseRootObject_2_register();
    TestCaseRootObject_1_register();
+
    TestCaseDummyTracer_1_register(); 
+
+   TestCaseEventRepository_1_register();
+   TestCaseEventRepository_2_register();
+   TestCaseEventRepository_3_register();
 }
