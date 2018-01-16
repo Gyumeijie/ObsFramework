1f7a4437819e4f3524734008697cde5f47be71aa
Gyumeijie <Gyumeijie@gmail.com>
2018-01-16 21:25:42 +0800

    add Checksum and a testcase, Checksum is needed in PUSMemoryDump, so add it first.

---
+++ src/RegressionTest/TestCaseChecksum_1.c
@@ -0,0 +1,159 @@
+//
+// Copyright 2004 P&P Software GmbH - All Rights Reserved
+//
+// TestCaseChecksum_1.c
+//
+// Version	1.0
+// Date		13.01.04
+// Author	A. Pasetti (P&P Software)
+
+#include "../GeneralInclude/CompilerSwitches.h"
+#include "../GeneralInclude/ClassId.h"
+#include "../GeneralInclude/Constants.h"
+#include "../Base/CC_RootObject.h"
+#include "../Utilities/Checksum.h"
+#include "TestCaseChecksum_1.h"
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                    non-pure virtual  method(s) definition
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Execute the test case. See class comment for details.
+ */
+static void runTestCase(void *obj)
+{
+
+   unsigned char data[10];
+   unsigned short result;
+
+   // Check the first stream of data
+   data[0] = 0;
+   data[1] = 0;
+
+   result = doChecksum(data, 2);
+   if (result != 0xFFFF) {	
+        TestCase_setTestResult((TestCase*)obj, TEST_FAILURE, 
+                                "Incorrect checksum result");
+		return;
+   }
+   if (!verifyChecksum(data, 2, result)) {	
+        TestCase_setTestResult((TestCase*)obj, TEST_FAILURE, 
+                                "Incorrect checksum verification");
+		return;
+   }
+
+   // Check the second stream of data
+   data[0] = 0;
+   data[1] = 0;
+   data[2] = 0;
+
+   result = doChecksum(data, 3);
+   if (result != 0xFFFF) {	
+        TestCase_setTestResult((TestCase*)obj, TEST_FAILURE, 
+                               "Incorrect checksum result");
+		return;
+   }
+   if (!verifyChecksum(data, 3, result)) {	
+        TestCase_setTestResult((TestCase*)obj, TEST_FAILURE, 
+                                "Incorrect checksum verification");
+		return;
+   }
+
+   // Check the third stream of data
+   data[0] = 0xAB;
+   data[1] = 0xCD;
+   data[2] = 0xEF;
+   data[3] = 0x01;
+
+   result = doChecksum(data, 4);
+   if (result != 0x9CF8) {	
+        TestCase_setTestResult((TestCase*)obj, TEST_FAILURE, 
+                                "Incorrect checksum result");
+		return;
+   }
+   if (!verifyChecksum(data, 4, result)) {	
+        TestCase_setTestResult((TestCase*)obj, TEST_FAILURE, 
+                                "Incorrect checksum verification");
+		return;
+   }  
+
+   verifyChecksum(data, 4, result) ;
+
+   // Check the fourth stream of data
+   data[0] = 0x14;
+   data[1] = 0x56;
+   data[2] = 0xF8;
+   data[3] = 0x9A;
+   data[4] = 0x00;
+   data[5] = 0x01;
+
+   result = doChecksum(data, 6);
+   if (result != 0x24DC) {	
+        TestCase_setTestResult((TestCase*)obj, TEST_FAILURE, 
+                                "Incorrect checksum result");
+		return;
+   }
+   if (!verifyChecksum(data, 6, result)) {	
+        TestCase_setTestResult((TestCase*)obj, TEST_FAILURE, 
+                                "Incorrect checksum verification");
+		return;
+   }
+
+   TestCase_setTestResult((TestCase*)obj, TEST_SUCCESS, 
+                            "Test Successful");
+   return;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                   object constructor and destructor
+//
+///////////////////////////////////////////////////////////////////////////////
+
+static void instance_init(Object *obj){}
+
+TestCaseChecksum_1* TestCaseChecksum_1_new(void)
+{
+    Object *obj = object_new(TYPE_TESTCASECHECKSUM_1);
+
+    TESTCASEGENERICSETUP_GET_CLASS(obj)->post_init((TestCaseGenericSetUp*)obj,
+                                                    ID_CHECKSUM*10+1,
+                                                    "TestCaseChecksum_1"); 
+    return (TestCaseChecksum_1*)obj;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                   binding and type registration
+//
+///////////////////////////////////////////////////////////////////////////////
+
+static void class_init(ObjectClass *oc, void *data)
+{
+    TestCaseClass *tcc = TESTCASE_CLASS(oc);
+    tcc->runTestCase = runTestCase;
+}
+
+static const TypeInfo type_info = {
+    .name = TYPE_TESTCASECHECKSUM_1,
+    .parent = TYPE_TESTCASEGENERICSETUP,
+    .instance_size = sizeof(TestCaseChecksum_1),
+    .abstract = false,
+    .class_size = sizeof(TestCaseChecksum_1Class),
+    .instance_init = instance_init,
+    .class_init = class_init,
+};
+
+void TestCaseChecksum_1_register(void)
+{
+    type_register_static(&type_info);
+}
---
+++ src/Utilities/Checksum.h
@@ -0,0 +1,66 @@
+//
+// Copyright 2004 P&P Software GmbH - All Rights Reserved
+//
+// Checksum.h 
+//
+// Version	1.0
+// Date		17.01.04 
+// Author	A. Pasetti
+//
+// Change Record:
+
+/**
+ * @file Checksum.h
+ * Utility functions to compute and verify a 16-bits ISO checksum. The checksum algorithms are
+ * as specified in appendix A.2 of the Packet Utilization Standard (issue of Jan.
+ * 30-th 2003). A checksum is computed by calling function <code>doChecksum</code>.
+ * It is verified by calling function <code>verifyChecksum</code>.
+ * The checksum is computed or verified over items that
+ * are treated as of type <code>unsigned char</code>. A checksum result is expressed as an
+ * <code>unsigned short</code> where the leftmost byte of the result (CK1) is the least significant
+ * byte of the return value of <code>doChecksum</code> and the rightmost byte (CK2) is the
+ * most significant byte.
+ * <p>
+ * The variable where the inermediate quantities C0 and C1 are accumulated is defined
+ * to be of type <code>int</code>. There is no check against an overflow in this
+ * variable. This essentially puts a limit on the maximum length of the data stream
+ * over which the checksum is computed.
+ * <p>
+ * The functions defined in this module do not conform to project-specific coding rule
+ * PR3.3. They take as argument an array holding the items over which the checksum is to be
+ * computer/verified. The alternative design is where the items are passed one by one and
+ * intermediate results are stored internally to the module. This alternative would have
+ * complied with PR3.3 but would also have been significantly slower for large chunks of data.
+ */
+
+static int C0;
+static int C1;
+static unsigned char CK1;
+static unsigned char CK2;
+
+/**
+ * Compute the checksum.
+ * The first argument is treated as an array of size n that holds the items over which
+ * the checksum is to be computed. The return value of the function is the checksum value.
+ * @param item the array holding the items that must be checksummed
+ * @param n the number of items in the array 
+ * @return the value of the checksum
+ * @version 1.0
+ * @author A. Pasetti
+ */
+unsigned short doChecksum(unsigned char *item, unsigned int n);
+
+/**
+ * Verify the checksum.
+ * The first argument is treated as an array of size n that holds the items over which
+ * the checksum is to be verified. Argument <code>checksum</code> holds the expected
+ * checksum value. The return value of the function indicates whether the checksum
+ * is verified (return value of true) or not (return value of false).
+ * @param item the array holding the items that must be checksum verified
+ * @param n the number of items in the array 
+ * @param checksum the value of the checksum
+ * @return the flag indicating whether the check sum is verified
+ * @version 1.0
+ * @author A. Pasetti
+ */
+bool verifyChecksum(unsigned char *item, unsigned int n, unsigned short checksum);
