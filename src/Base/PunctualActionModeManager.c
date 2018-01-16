49cda5699f74ab0b936c3a323401819dbf7f2990
Gyumeijie <Gyumeijie@gmail.com>
2018-01-16 09:52:11 +0800

    feat: add PunctualActionModeManager

---
+++ src/Base/PunctualActionModeManager.c
@@ -0,0 +1,128 @@
+//
+// Copyright 2004 P&P Software GmbH - All Rights Reserved
+//
+// PunctualActionModeManager.c
+//
+// Version	1.0
+// Date		29.01.04
+// Author	R. Totaro, A. Pasetti
+
+#include "../GeneralInclude/CompilerSwitches.h"
+#include "PunctualActionModeManager.h"
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                    pure virtual  method(s) definition
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Iteration method to iterate through the items in the set of
+ * punctual actions applicable to the current operational mode. 
+ * This method should be used jointly with methods: <code>next</code>, 
+ * <code>isIterationFinished</code> and <code>getIterationPunctualAction</code>. 
+ * Taken together, 
+ * these methods allow all the items in the currently applicable set 
+ * of punctual actions to be scanned. 
+ * During an iteration cycle, an internal pointer, the <i>iteration
+ * pointer</i>, is made to scan over all the items in the currently applicable
+ * set of telemetry packets.
+ * The item currently pointed at by the iteration counter can be retrieved
+ * through method <code>getIterationPunctualAction</code>.
+ * Thus, a typical iteration cycle might be organized as follows: <PRE>
+ *      for(first(void); !isIterationFinished(void); next(void)) {
+ *          pItem = getIterationPunctualAction(void);
+ *          . . .    // process pItem
+ *      }  </PRE>
+ * @see #getIterationItem
+ */
+static void first(void *obj)
+{
+    // this is automate genenrated by converter
+}
+
+/**
+ * Iteration method to be used in conjunction with methods <code>first</code>
+ * and <code>isIterationFinished</code>.
+ * @see #first
+ */
+static void next(void *obj)
+{
+    // this is automate genenrated by converter
+}
+
+/**
+ * Iteration method to be used in conjunction with methods <code>first</code>
+ * and <code>next</code>.
+ * This method should only be used as part of an iteration cycle. The result
+ * of calling it before ever calling <code>first</code>
+ * is undefined.
+ * @see #first
+ * @return true if the end of the iteration has been reached, false otherwise
+ */
+static bool isIterationFinished(void *obj)
+{
+    // this is automate genenrated by converter
+}
+
+/**
+ * Return the punctual action item pointed at by the iterator.
+ * This method is a type-safe wrapper for method <code>getIteratorItem</code>
+ * in the superclass.
+ * @see ListModeManager#getIterationItem
+ * @return the punctual action item pointed at by the iterator
+ */
+static PunctualAction* getIterationPunctualAction(void *obj)
+{
+    // this is automate genenrated by converter
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                   object constructor and destructor
+//
+///////////////////////////////////////////////////////////////////////////////
+
+static void instance_init(Object *obj){}
+
+PunctualActionModeManager* PunctualActionModeManager_new(void)
+{
+    return (PunctualActionModeManager*)object_new(TYPE_PUNCTUALACTIONMODEMANAGER);
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                   binding and type registration
+//
+///////////////////////////////////////////////////////////////////////////////
+
+static void class_init(ObjectClass *oc, void *data)
+{
+    PunctualActionModeManagerClass *pammc = PUNCTUALACTIONMODEMANAGER_CLASS(oc);
+
+    pammc->first = first;
+    pammc->next = next;
+    pammc->isIterationFinished = isIterationFinished;
+    pammc->getIterationPunctualAction = getIterationPunctualAction;
+}
+
+static const TypeInfo type_info = {
+    .name = TYPE_PUNCTUALACTIONMODEMANAGER,
+    .parent = TYPE_MODEMANAGER,
+    .instance_size = sizeof(PunctualActionModeManager),
+    .abstract = true,
+    .class_size = sizeof(PunctualActionModeManagerClass),
+    .instance_init = instance_init,
+    .class_init = class_init,
+};
+
+void PunctualActionModeManager_register(void)
+{
+    type_register_static(&type_info);
+}
---
+++ src/Base/PunctualActionModeManager.h
@@ -0,0 +1,85 @@
+//
+// Copyright 2004 P&P Software GmbH - All Rights Reserved
+//
+// PunctualActionModeManager.h
+//
+// Version	1.0
+// Date		29.02.04 
+// Author	A. Pasetti, R. Totaro
+//
+// Change Record:
+
+
+#ifndef PUNCTUALACTIONMODEMANAGER_H
+#define PUNCTUALACTIONMODEMANAGER_H
+
+
+#include "ModeManager.h"
+#include "PunctualAction.h"
+#include "../Qom/object.h"
+
+
+/*
+ * Base abstract class for mode manager components for the punctual
+ * action manager.
+ * A punctual action mode manager handles one single <i>strategy</i>, which
+ * is represented by a set of <i>punctual action</i> components.
+ * To every operational mode, the telemetry mode manager associates 
+ * a set of punctual actions. 
+ * Client access the items in this set through four
+ * <i>iteration methods</i>: <code>first</code>, <code>next</code>,
+ * <code>isIterationFinished</code>, and <code>getPunctualAction</code>.
+ * <p>
+ * This is an abstract class because it specifies neither the implementation
+ * mechanism for the set of punctual actions nor the mode update mechanism.
+ * @todo Create a class DC_FSMPunctualActionModeManager where the mode is driven
+ * by an FSM. Same thing should be done for the TelemetryModeManager
+ * @see CC_PunctualActionManager
+ * @see PunctualAction
+ * @author Alessandro Pasetti 
+ * @author Roberto Totaro
+ * @version 1.0
+ * @ingroup ModeManagement
+ */
+#define TYPE_PUNCTUALACTIONMODEMANAGER "punctualactionmodemanager"
+
+void PunctualActionModeManager_register(void);
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                            class and struct
+//
+///////////////////////////////////////////////////////////////////////////////
+
+struct PunctualActionModeManager {
+    ModeManager parent;
+};
+
+
+struct PunctualActionModeManagerClass {
+    ModeManagerClass parent_class;
+ 
+    void (*first)(void *obj);
+    void (*next)(void *obj);
+    bool (*isIterationFinished)(void *obj);
+    PunctualAction* (*getIterationPunctualAction)(void *obj);
+};
+
+
+#define PUNCTUALACTIONMODEMANAGER_GET_CLASS(obj) \
+        OBJECT_GET_CLASS(PunctualActionModeManagerClass,\
+                                           obj, TYPE_PUNCTUALACTIONMODEMANAGER)
+
+#define PUNCTUALACTIONMODEMANAGER_CLASS(klass) \
+        OBJECT_CLASS_CHECK(PunctualActionModeManagerClass,\
+                                         klass, TYPE_PUNCTUALACTIONMODEMANAGER)
+
+#define PUNCTUALACTIONMODEMANAGER(obj) \
+        OBJECT_CHECK(PunctualActionModeManager,\
+                                           obj, TYPE_PUNCTUALACTIONMODEMANAGER)
+
+
+PunctualActionModeManager* PunctualActionModeManager_new(void);
+
+#endif
