a1a38518be91d080ed467587ec9676e58b21c3de
Gyumeijie <Gyumeijie@gmail.com>
2018-01-21 19:45:27 +0800

    add DC_PUSTcVerificationPacket but add a testcase for it later.

--- src/Telemetry/TelemetryTypeRegister.c
+++ src/Telemetry/TelemetryTypeRegister.c
@@ -2,6 +2,7 @@
 #include "TelemetryPacket.h"
 #include "DC_DummyTelemetryPacket.h"
 #include "PUSTelemetryPacket.h"
+#include "DC_PUSTcVerificationPacket.h"
 #include "DC_DummyPUSTelemetryPacket.h"
 #include "PUSMemoryDump.h"
 #include "DC_PUSMemoryDumpOffset.h"
@@ -24,6 +25,7 @@ void telemetry_type_register(void)
     TelemetryPacket_register();
     DC_DummyTelemetryPacket_register();
     PUSTelemetryPacket_register();
+    DC_PUSTcVerificationPacket_register();
     DC_DummyPUSTelemetryPacket_register();
     PUSMemoryDump_register();
     DC_PUSMemoryDumpOffset_register();
