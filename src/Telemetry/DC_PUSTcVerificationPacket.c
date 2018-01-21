505ceb5334f00024f3c27ccf7bbcb977e7a99174
Gyumeijie <Gyumeijie@gmail.com>
2018-01-21 19:45:27 +0800

    feat: add DC_PUSTcVerificationPacket
    a testcase will be added later.

---
+++ src/Telemetry/DC_PUSTcVerificationPacket.c
@@ -0,0 +1,225 @@
+//
+// Copyright 2004 P&P Software GmbH - All Rights Reserved
+//
+// DC_DummyPUSTelemetryPacket.cpp
+//
+// Version	1.0
+// Date		04.04.03
+// Author	A. Pasetti (P&P Software)
+
+#include "../GeneralInclude/CompilerSwitches.h"
+#include "../GeneralInclude/TestConstants.h"
+#include "../GeneralInclude/DebugSupport.h"
+#include "../GeneralInclude/ClassId.h"
+#include "../GeneralInclude/Constants.h"
+#include "../Base/CC_RootObject.h"
+#include "../Telecommand/PUSTelecommand.h"
+#include "../Telemetry/PUSTelemetryPacket.h"
+#include "../Telemetry/DC_PUSTcVerificationPacket.h"
+
+#include <string.h>
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                  non-virtual member method(s) definition
+//
+///////////////////////////////////////////////////////////////////////////////
+
+TD_CheckCode DC_PUSTcVerificationPacket_getErrorCode
+(
+    DC_PUSTcVerificationPacket *This
+)
+{
+    TD_CheckCode temp;
+    memcpy(&temp, This->tmByte + sizeof(TD_PUSPacketId) + sizeof(TD_TelecommandId),
+           sizeof(TD_CheckCode));
+    return temp;
+}
+
+void DC_PUSTcVerificationPacket_setErrorCode
+(
+    DC_PUSTcVerificationPacket *This, 
+    TD_CheckCode errCode
+)
+{
+    assert(errCode > 0);
+    memcpy(This->tmByte + sizeof(TD_PUSPacketId) + sizeof(TD_TelecommandId),
+           &errCode, sizeof(TD_CheckCode));
+}
+
+TD_TelecommandId DC_PUSTcVerificationPacket_getTelecommandId
+(
+    DC_PUSTcVerificationPacket *This
+)
+{
+    TD_TelecommandId temp;
+    memcpy(&temp, This->tmByte + sizeof(TD_PUSPacketId), sizeof(TD_TelecommandId));
+    return temp;
+}
+
+void DC_PUSTcVerificationPacket_setTelecommandId
+(
+    DC_PUSTcVerificationPacket *This, 
+    TD_TelecommandId tcId
+)
+{
+    assert(tcId>0);
+    memcpy(This->tmByte + sizeof(TD_PUSPacketId), &tcId, sizeof(TD_TelecommandId));
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                    non-pure virtual  method(s) definition
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Return the number of bytes managed by this telemetry packet.
+ * The number of bytes is computed as follows. If the telemetry packet
+ * is being used to report a successful telecommand execution, then its
+ * number of bytes is given by: <pre>
+ *      sizeof(TD_PUSPacketId)+sizeof(TD_TelecommandId) </pre>
+ * If the telemetry packet
+ * is being used to report a successful telecommand execution, then its
+ * number of bytes is given by: <pre>
+ *      sizeof(TD_PUSPacketId)+sizeof(TD_TelecommandId)+sizeof(TD_CheckCode) </pre>
+ * Note that <code>TD_CheckCode</code> is the type of the error code field.
+ * @see #setErrorCode
+ * @return number of bytes in this telemetry packet
+ */
+static unsigned int getNumberOfBytes(void *obj)
+{
+    DC_PUSTcVerificationPacket *This = DC_PUSTCVERIFICATIONPACKET(obj);
+    TelemetryPacketClass *tpc = TELEMETRYPACKET_GET_CLASS(obj);
+
+    assert(tpc->getSubType(This) != 0);
+
+    TD_TelecommandSubType tcSubType = tpc->getSubType(This);
+    if ((tcSubType == PUS_ST_TC_VER_ACC_SC) ||
+        (tcSubType == PUS_ST_TC_EXE_STR_SC) ||
+        (tcSubType == PUS_ST_TC_EXE_PRO_SC) ||
+        (tcSubType == PUS_ST_TC_EXE_END_SC)) {
+         return This->packetLengthSuccess;
+    } else {
+         return This->packetLengthFailure;
+    }
+}
+
+/**
+ * Implement the  <i>data acquisition service</i> for the PUS telecommand verification
+ * packet. The bytes associated to this telemetry packet are returned according to the
+ * following algorithm: <pre>
+ *      if((subtype == PUS_ST_TC_VER_ACC_SC) ||
+ *           (subtype == PUS_ST_TC_EXE_STR_SC) ||
+ *           (subtype == PUS_ST_TC_EXE_PRO_SC) ||
+ *           (subtype == PUS_ST_TC_EXE_END_SC)) {
+ *          return TC packet ID;
+ *          return TC packet sequence control;
+ *       } else {
+ *          return TC packet ID;
+ *          return TC packet sequence control;
+ *          return error code;
+ *       } </pre>
+ * The TC packet ID, TC packet sequence control, and the error code are transferred byte
+ * by byte starting from the byte with the highest address.
+ * @return value of n-th telemetry byte
+ * @param n the telemetry byte to be returned(starting with byte 0)
+ */
+static unsigned char getUnsignedByte(void *obj, unsigned int n)
+{
+    DC_PUSTcVerificationPacket *This = DC_PUSTCVERIFICATIONPACKET(obj);
+    return This->tmByte[n];
+}
+
+/**
+ * Implement the <i>data update service</i> for the PUS telecommand verification packet.
+ * This type of packet is configured dynamically by the component that monitors the
+ * execution status of the telecommands. Its update service is therefore implemented
+ * as a dummy method that returns without taking any action.
+ */
+static void update(void *obj)
+{
+    return;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                   object constructor and destructor
+//
+///////////////////////////////////////////////////////////////////////////////
+
+static void instance_init(Object *obj)
+{
+    DC_PUSTcVerificationPacket *This = DC_PUSTCVERIFICATIONPACKET(obj);
+    TelemetryPacketClass *tpc = TELEMETRYPACKET_GET_CLASS(obj);
+
+    This->packetLengthSuccess = sizeof(TD_PUSPacketId) + sizeof(TD_TelecommandId);
+    This->packetLengthFailure = sizeof(TD_PUSPacketId) +
+                                sizeof(TD_TelecommandId) + sizeof(TD_CheckCode);
+    This->tmByte = g_malloc(sizeof(unsigned char)*This->packetLengthFailure);
+
+    tpc->setType(obj, PUS_TYPE_TC_VER);
+
+    // The PUS packet identifier, the telecommand identifier and the error
+    // code are stored in the tmByte array
+
+    // set the PUS packet identifier
+    TD_PUSPacketId pidTemp = PUSTelecommand_getPacketId();
+    memcpy(This->tmByte, &pidTemp, sizeof(TD_PUSPacketId));
+
+    // initialize the telecommand identifier
+    TD_TelecommandId tcidTemp = 0;
+    memcpy(This->tmByte + sizeof(TD_PUSPacketId), &tcidTemp, sizeof(TD_TelecommandId));
+
+    // initialize the error code
+    TD_CheckCode ccTemp = 0;
+    memcpy(This->tmByte + sizeof(TD_PUSPacketId) + sizeof(TD_TelecommandId),
+           &ccTemp, sizeof(TD_CheckCode));
+
+
+    CC_RootObject_setClassId((CC_RootObject*)obj, ID_PUSTCVERIFICATIONPACKET);
+}
+
+DC_PUSTcVerificationPacket* DC_PUSTcVerificationPacket_new(void)
+{
+    Object *obj = object_new(TYPE_DC_PUSTCVERIFICATIONPACKET);
+    return (DC_PUSTcVerificationPacket*)obj;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                   binding and type registration
+//
+///////////////////////////////////////////////////////////////////////////////
+
+static void class_init(ObjectClass *oc, void *data)
+{
+    TelemetryPacketClass *tpc = TELEMETRYPACKET_CLASS(oc);
+
+    tpc->getNumberOfBytes = getNumberOfBytes;
+    tpc->getUnsignedByte = getUnsignedByte;
+    tpc->update = update;
+}
+
+static const TypeInfo type_info = {
+    .name = TYPE_DC_PUSTCVERIFICATIONPACKET,
+    .parent = TYPE_PUSTELEMETRYPACKET,
+    .instance_size = sizeof(DC_PUSTcVerificationPacket),
+    .abstract = false,
+    .class_size = sizeof(DC_PUSTcVerificationPacketClass),
+    .instance_init = instance_init,
+    .class_init = class_init,
+};
+
+void DC_PUSTcVerificationPacket_register(void)
+{
+    type_register_static(&type_info);
+}
---
+++ src/Telemetry/DC_PUSTcVerificationPacket.h
@@ -0,0 +1,135 @@
+//
+// Copyright 2004 P&P Software GmbH - All Rights Reserved
+//
+// DC_PUSTcVerificationPacket.h
+//
+// Version	1.0
+// Date		01.12.02
+// Author	A. Pasetti(P&P Software)
+
+
+#ifndef DC_PUSTCVERIFICATIONPACKET_H
+#define DC_PUSTCVERIFICATIONPACKET_H
+
+
+#include "../GeneralInclude/ForwardDeclarations.h"
+#include "../GeneralInclude/BasicTypes.h"
+#include "../Telemetry/PUSTelemetryPacket.h"
+#include "../Qom/object.h"
+
+
+/*
+ * PUS telemetry packet implementing the telecommand verification service.
+ * This class can model any of the telecommand verification service
+ * packets. The value of the subtype attribute determines which packet is
+ * being modeled.
+ * <p>
+ * This class is intended to be used by a component that monitors
+ * the execution status of a telecommand. Upon detecting a change in
+ * the execution status of a telecommand, this component configures an
+ * instance of <code>PUSTcVerificationPacket</code> in accordance with the
+ * detected state of the telecommand execution. The <code>PUSTcVerificationPacket</code>
+ * instance should then be loaded into the <i>telementry manager</i> for immediate
+ * dispatch to the telemetry channel.
+ * <p>
+ * A telemetry packet of type <code>PUSTcVerificationPacket</code> can contain two or
+ * three fields. It contains two fields if it reports a telecommand success. It contains three fields
+ * if it reports a telecommand failure. The two fields that are always present are
+ * the packet identifier of the telecommand and the packet sequence control of the telecommand.
+ * The third field is a single error code that conveys information about the reason of
+ * the telecommand failure.
+ * @todo construct a test case for this class.
+ * @author Alessandro Pasetti(P&P Software GmbH)
+ * @version 1.0
+ */
+#define TYPE_DC_PUSTCVERIFICATIONPACKET "dc_pustcverificationpacket"
+
+void DC_PUSTcVerificationPacket_register(void);
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                            class and struct
+//
+///////////////////////////////////////////////////////////////////////////////
+
+typedef struct DC_PUSTcVerificationPacket {
+    PUSTelemetryPacket parent;
+
+    unsigned int packetLengthSuccess;
+    unsigned int packetLengthFailure;
+    unsigned char *tmByte;
+} DC_PUSTcVerificationPacket;
+
+
+typedef struct DC_PUSTcVerificationPacketClass {
+    PUSTelemetryPacketClass parent_class;
+} DC_PUSTcVerificationPacketClass;
+
+
+#define DC_PUSTCVERIFICATIONPACKET_GET_CLASS(obj) \
+        OBJECT_GET_CLASS(DC_PUSTcVerificationPacketClass, \
+                                          obj, TYPE_DC_PUSTCVERIFICATIONPACKET)
+
+#define DC_PUSTCVERIFICATIONPACKET_CLASS(klass) \
+        OBJECT_CLASS_CHECK(DC_PUSTcVerificationPacketClass, \
+                                        klass, TYPE_DC_PUSTCVERIFICATIONPACKET)
+
+#define DC_PUSTCVERIFICATIONPACKET(obj) \
+        OBJECT_CHECK(DC_PUSTcVerificationPacket, \
+                                          obj, TYPE_DC_PUSTCVERIFICATIONPACKET)
+
+
+DC_PUSTcVerificationPacket* DC_PUSTcVerificationPacket_new(void);
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                 non-virtual member method(s) declaration
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Return the error code that describes the reason for the telecommand failure.
+ * This field is only meaningful when the telemetry packet is being used to
+ * describe a telecommand failure. The default value of the error code is zero.
+ * @return the error code
+ */
+TD_CheckCode DC_PUSTcVerificationPacket_getErrorCode
+(
+    DC_PUSTcVerificationPacket *This
+);
+
+/**
+ * Set the error code that describes the reason for the telecommand failure.
+ * @see #getErrorCode
+ * @param errCode the error code
+ */
+void DC_PUSTcVerificationPacket_setErrorCode
+(
+    DC_PUSTcVerificationPacket *This, 
+    TD_CheckCode errCode
+);
+
+/**
+ * Return the identifier of the telecommand for which the execution verification
+ * report is constructed.
+ * @return the telecommand identifier
+ */
+TD_TelecommandId DC_PUSTcVerificationPacket_getTelecommandId
+(
+    DC_PUSTcVerificationPacket *This
+);
+
+/**
+ * Set the identifier of the telecommand for which the execution verification
+ * report is constructed.
+ * @param tcId the telecommand identifier
+ */
+void DC_PUSTcVerificationPacket_setTelecommandId
+(
+    DC_PUSTcVerificationPacket *This, TD_TelecommandId tcId
+);
+
+
+#endif
