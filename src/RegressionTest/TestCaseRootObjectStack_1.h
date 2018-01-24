af175de6d8314c1bb1561a2b88b836a18ea38505
Gyumeijie <Gyumeijie@gmail.com>
2018-01-24 11:51:28 +0800

    feat: add CC_RootObjectStack and it's unit testcase

---
+++ src/RegressionTest/TestCaseRootObjectStack_1.h
@@ -0,0 +1,87 @@
+//
+// Copyright 2003 P&P Software GmbH - All Rights Reserved
+//
+// TestCaseRootObjectStack_1.h
+//
+// Version	1.0
+// Date		10.10.03 (Version 1.0)
+// Author	A. Pasetti(P&P Software)
+//
+
+
+#ifndef TESTCASEROOTOBJECTSTACK_1_H
+#define TESTCASEROOTOBJECTSTACK_1_H
+
+
+#include "../Utilities/TestCaseWithEvtCheck.h"
+#include "../Qom/object.h"
+
+
+/*
+ * Check the functionality of the container class for items of type
+ * pointer to <code>CC_RootObject*</code>.
+ * One instance of the container class is created and
+ * the following specific checks are performed upon it : 
+ * The correctness of the class identifier is checked.
+ * It is checked that the configuration check service
+ * initially reports "not configured".
+ * The stack size is set to 2 and it is checked that the set value
+ * is correctly reported.
+ * It is checked that the configuration check service reports
+ * "configured".
+ * An item is pushed onto the stack and it is checked that the
+ * stack reports "number of items on the stack equals 1".
+ * An item is popped from the stack and it is checked that the
+ * correct element is returned.
+ * A pop operation is performed and it is verified that pNULL
+ * is returned and the correct event report is generated.
+ * Three items are pushed on the stack and it is verified that
+ * only 2 are actually placed on the stack and that the correct event
+ * report is generated.
+ * It is verified that the stack is full after the second push operation.
+ * A reset operation is performed and it is verified that
+ * the number of items on the stack is zero and that the stack is
+ * empty.
+ * 
+ * This class was generated automatically by an XSLT program.
+ * @see CC_RootObjectStack
+ * @author Alessandro Pasetti(P&P Software GmbH)
+ * @version 1.0
+ */
+#define TYPE_TESTCASEROOTOBJECTSTACK_1 "testcaserootobjectstack_1"
+
+void TestCaseRootObjectStack_1_register(void);
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                            class and struct
+//
+///////////////////////////////////////////////////////////////////////////////
+
+typedef struct TestCaseRootObjectStack_1 {
+    TestCaseWithEvtCheck parent;
+} TestCaseRootObjectStack_1;
+
+
+typedef struct TestCaseRootObjectStack_1Class {
+    TestCaseWithEvtCheckClass parent_class;
+} TestCaseRootObjectStack_1Class;
+
+
+#define TESTCASEROOTOBJECTSTACK_1_GET_CLASS(obj) \
+        OBJECT_GET_CLASS(TestCaseRootObjectStack_1Class, \
+                                           obj, TYPE_TESTCASEROOTOBJECTSTACK_1)
+
+#define TESTCASEROOTOBJECTSTACK_1_CLASS(klass) \
+        OBJECT_CLASS_CHECK(TestCaseRootObjectStack_1Class, \
+                                         klass, TYPE_TESTCASEROOTOBJECTSTACK_1)
+
+#define TESTCASEROOTOBJECTSTACK_1(obj) \
+        OBJECT_CHECK(TestCaseRootObjectStack_1, \
+                                           obj, TYPE_TESTCASEROOTOBJECTSTACK_1)
+
+
+TestCaseRootObjectStack_1* TestCaseRootObjectStack_1_new(void);
+
+#endif
---
+++ src/System/CC_RootObjectStack.c
@@ -0,0 +1,180 @@
+//
+// Copyright 2003 P&P Software GmbH - All Rights Reserved
+//
+// CC_RootObjectStack.h
+//
+// Version	1.0
+// Date		30.08.03 (Version 1.0)
+// Author	A. Pasetti (P&P Software)
+//
+
+#include "../GeneralInclude/CompilerSwitches.h"
+#include "../GeneralInclude/DebugSupport.h"
+#include "../GeneralInclude/ClassId.h"
+#include "../GeneralInclude/Constants.h"
+#include "../Event/DC_EventRepository.h"
+#include "../Base/CC_RootObject.h"
+#include "../Base/CC_RootObject.h"
+#include "CC_RootObjectStack.h"
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                  non-virtual member method(s) definition
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void CC_RootObjectStack_push(CC_RootObjectStack *This, CC_RootObject *newItem)
+{
+   assert((This->pStack != pNULL));
+
+   if (CC_RootObjectStack_isFull(This)) {
+       DC_EventRepository *dc_er = CC_RootObject_getEventRepository();
+       DC_EventRepositoryClass *dc_erc = DC_EVENTREPOSITORY_GET_CLASS(dc_er);
+       dc_erc->create(dc_er, (CC_RootObject*)This, EVT_STACK_FULL);
+   } else {
+       This->pStack[This->stackPointer] = newItem;
+       This->stackPointer++;
+   }
+
+   assert(This->stackPointer <= This->size);
+}
+
+CC_RootObject* CC_RootObjectStack_pop(CC_RootObjectStack *This)
+{
+   assert((This->pStack != pNULL));
+
+   if (CC_RootObjectStack_isEmpty(This)) {
+       DC_EventRepository *dc_er = CC_RootObject_getEventRepository();
+       DC_EventRepositoryClass *dc_erc = DC_EVENTREPOSITORY_GET_CLASS(dc_er);
+       dc_erc->create(dc_er, (CC_RootObject*)This, EVT_STACK_EMPTY);
+       return pNULL;
+   } else {
+       This->stackPointer--;
+       return This->pStack[This->stackPointer];
+   }
+}
+
+void CC_RootObjectStack_setStackSize(CC_RootObjectStack *This, unsigned int size)
+{
+   assert(This->size == 0);      // should not be called more than once
+   assert(size > 0);             // stack size must be greater than zero
+
+   This->pStack = g_malloc(size*sizeof(CC_RootObject*));
+   assert(This->pStack != pNULL);
+   This->size = size;
+   This->stackPointer = 0;
+}
+
+unsigned int CC_RootObjectStack_getStackSize(const CC_RootObjectStack *This)
+{
+   return This->size;
+}
+
+unsigned int CC_RootObjectStack_getNumberOfItems(const CC_RootObjectStack *This)
+{
+    return This->stackPointer;
+}
+
+bool CC_RootObjectStack_isEmpty(const CC_RootObjectStack *This)
+{
+   assert((This->pStack != pNULL));
+   return (This->stackPointer == 0);
+}
+
+bool CC_RootObjectStack_isFull(const CC_RootObjectStack *This)
+{
+   assert((This->pStack != pNULL));
+   return (This->stackPointer == This->size);
+}
+
+void CC_RootObjectStack_reset(CC_RootObjectStack *This)
+{
+   assert((This->pStack != pNULL));
+   This->stackPointer = 0;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                    non-pure virtual  method(s) definition
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Perform a class-specific configuration check on a stack object:
+ * verify that the stack size
+ * has a legal value.
+ * @return true if the object is configured, false otherwise
+ */
+static bool isObjectConfigured(void *obj)
+{
+    CC_RootObjectClass *cc_roc = GET_CLASS(TYPE_CC_ROOTOBJECT);
+    CC_RootObjectStack *This = CC_ROOTOBJECTSTACK(obj);
+
+    // Check configuration of super object
+    if (!cc_roc->isObjectConfigured(obj)) {
+        return NOT_CONFIGURED;
+    }
+
+    if ((This->pStack == pNULL)) {
+        return NOT_CONFIGURED;
+    }
+
+    return CONFIGURED;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                   object constructor and destructor
+//
+///////////////////////////////////////////////////////////////////////////////
+
+static void instance_init(Object *obj)
+{
+    CC_RootObjectStack *This = CC_ROOTOBJECTSTACK(obj);
+    This->size = 0;
+    This->stackPointer = 0;
+    This->pStack = pNULL;
+
+	CC_RootObject_setClassId((CC_RootObject*)obj, ID_ROOTOBJECTSTACK);
+}
+
+CC_RootObjectStack* CC_RootObjectStack_new(void)
+{
+    Object *obj = object_new(TYPE_CC_ROOTOBJECTSTACK);
+    return (CC_RootObjectStack*)obj;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                   binding and type registration
+//
+///////////////////////////////////////////////////////////////////////////////
+
+static void class_init(ObjectClass *oc, void *data)
+{
+    CC_RootObjectClass *cc_roc = CC_ROOTOBJECT_CLASS(oc);
+    cc_roc->isObjectConfigured = isObjectConfigured;
+}
+
+static const TypeInfo type_info = {
+    .name = TYPE_CC_ROOTOBJECTSTACK,
+    .parent = TYPE_CC_ROOTOBJECT,
+    .instance_size = sizeof(CC_RootObjectStack),
+    .abstract = false,
+    .class_size = sizeof(CC_RootObjectStackClass),
+    .instance_init = instance_init,
+    .class_init = class_init,
+};
+
+void CC_RootObjectStack_register(void)
+{
+    type_register_static(&type_info);
+}
