91caff82db7b842f08d9287ff959abbda19f0957
Gyumeijie <Gyumeijie@gmail.com>
2018-01-25 13:34:55 +0800

    feat: add DC_PUSTelemetryModeManager and it's unit testcase
    but there seems to be something wrong with uninitialized timeTag of telemetry packet, I will figure out and fix it later.

---
+++ src/RegressionTest/TestCasePUSTelemetryModeManager_1.h
@@ -0,0 +1,103 @@
+//
+// Copyright 2003 P&amp;P Software GmbH(Konstanz, Germany) - All Rights Reserved
+//
+// TestCasePUSTelemetryModeManager_1.h
+//
+// Version	1.0
+// Date		11.11.03
+// Author	A. Pasetti(P&amp;P Software)
+
+
+#ifndef TESTCASEPUSTELEMETRYMODEMANAGER_1_H
+#define TESTCASEPUSTELEMETRYMODEMANAGER_1_H
+
+
+#include "../Utilities/TestCasePUSFull.h"
+#include "../Qom/object.h"
+
+
+/*
+ * Check the functionality of the <code>DC_PUSTelemetryModeManager</code>
+ * class. The test case is configured as follows:<ul>
+ * <li>The telemetry packet factory is retrieved.</li>
+ * <li>An instance of class <code>DC_PUSTelemetryModeManager</code> is created.</li>
+ * <li>Two data reporting packet instances(instances of class
+ * <code>DC_PUSDataReportingPacket</code>) are retrieved from the telemetry packet 
+ * factory. The SIDs of the telemetry packets are SID_A and
+ * SID_B. </li>
+ * </ul>
+ * The following specific tests are then performed:<ol>
+ * <li>The correctness of the class identifier is checked.</li>
+ * <li>It is checked that the mode manager component is initially not configured.</li>
+ * <li>The maximum number of packets N allowed by the mode manager is retrieved 
+ * (recall that the telemetry mode manager is a 
+ * singleton class whose single instance is configured in another test case)</li>
+ * <li>It is checked that the mode manager component is now configured.</li>
+ * <li>The two packets are loaded in the mode manager and it is checked that 
+ * the number of loaded packets is correctly reported.</li>
+ * <li>The SID_B packet is loaded N-2 times(N being the capacity of the 
+ * telemetry mode manager) and it is checked that the component reports: 
+ * "mode manager is full". </li>
+ * <li>An attempt is made to load a further packet and it is checked that this
+ * results in the correct event report being generated.</li>
+ * <li>The iterator service is checked.</li>
+ * <li>The mode manager is asked to verify whether a packet with SID other
+ * than SID_A or SID_B is loaded and it is verified that the return value is: "false".</li>
+ * <li>Packet SID_A is unloaded and the success of the unload operation is checked.</li>
+ * <li>It is attempted to unload a non-existent packet and it is checked that 
+ * the correct event is created.</li>
+ * <li>The iterator service is checked again.</li>
+ * <li>Packet SID_A loaded again and then packet SID_B is unloaded 
+ * and the 
+ * success of the load/unload operations is checked.</li>
+ * <li>The iterator service is checked again.</li>
+ * <li>Packet SID_B is unloaded(the mode manager should now be empty)
+ * and the 
+ * success of the unload operation is checked.</li>
+ * <li>The iterator service is checked again.</li>
+ * </ol>
+ * Note that this test case uses the telemetry packet factory. It assumes that the structure
+ * of the factory is such as to allow two packets of type <code>DC_PUSDataReportingPacket</code>
+ * to be loaded in the factory.
+ * @see DC_PUSTelemetryModeManager
+ * @see CC_TelemetryPacketFactory
+ * @author Alessandro Pasetti(P&amp;P Software GmbH)
+ * @version 1.0
+ */
+#define TYPE_TESTCASEPUSTELEMETRYMODEMANAGER_1 "testcasepustelemetrymodemanager_1"
+
+void TestCasePUSTelemetryModeManager_1_register(void);
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                            class and struct
+//
+///////////////////////////////////////////////////////////////////////////////
+
+typedef struct TestCasePUSTelemetryModeManager_1 {
+    TestCasePUSFull parent;
+} TestCasePUSTelemetryModeManager_1;
+
+
+typedef struct TestCasePUSTelemetryModeManager_1Class {
+    TestCasePUSFullClass parent_class;
+} TestCasePUSTelemetryModeManager_1Class;
+
+
+#define TESTCASEPUSTELEMETRYMODEMANAGER_1_GET_CLASS(obj) \
+        OBJECT_GET_CLASS(TestCasePUSTelemetryModeManager_1Class, \
+                                   obj, TYPE_TESTCASEPUSTELEMETRYMODEMANAGER_1)
+
+#define TESTCASEPUSTELEMETRYMODEMANAGER_1_CLASS(klass) \
+        OBJECT_CLASS_CHECK(TestCasePUSTelemetryModeManager_1Class, \
+                                 klass, TYPE_TESTCASEPUSTELEMETRYMODEMANAGER_1)
+
+#define TESTCASEPUSTELEMETRYMODEMANAGER_1(obj) \
+        OBJECT_CHECK(TestCasePUSTelemetryModeManager_1, \
+                                   obj, TYPE_TESTCASEPUSTELEMETRYMODEMANAGER_1)
+
+
+TestCasePUSTelemetryModeManager_1* TestCasePUSTelemetryModeManager_1_new(void);
+
+#endif
---
+++ src/Telemetry/DC_PUSTelemetryModeManager.h
@@ -0,0 +1,171 @@
+//
+// Copyright 2004 P&P Software GmbH - All Rights Reserved
+//
+// DC_PUSTelemetryModeManager.h
+//
+// Version	1.0
+// Date		23.01.04
+// Author	A. Pasetti(P&P Software)
+
+
+#ifndef DC_PUSTELEMETRYMODEMANAGER_H
+#define DC_PUSTELEMETRYMODEMANAGER_H
+
+
+#include "../GeneralInclude/BasicTypes.h"
+#include "../GeneralInclude/ForwardDeclarations.h"
+#include "TelemetryModeManager.h"
+#include "../Qom/object.h"
+
+
+/*
+ * PUS telemetry mode manager.
+ * This telemetry mode manager has one single mode to which there 
+ * corresponds a set of PUS data reporting telemetry packets 
+ * (instances of class <code>PUSDataReportingPacket</code>).
+ * The telemetry packets can be loaded and unloaded dynamically. 
+ * The iteration methods iterate through all the currently loaded 
+ * telemetry packets(the order of the iteration is undefined).
+ * The mode manager is configured with the maximum number of 
+ * telemetry packets that it can hold(method <code>setMaxNumberOfPackets</code>).
+ * </p>
+ * An initialization sequence for this class consists of the following
+ * steps(to be performed in the order given): <ul>
+ * <li>Set the maximum number of loadable packets with method <code>setMaxNumberOfPackets</code></li>
+ * <li>Reset the component by calling method <code>reset</code></li>
+ * </ul>
+ * @ingroup PUS
+ * @author Alessandro Pasetti(P&P Software GmbH)
+ * @version 1.0
+ */
+#define TYPE_DC_PUSTELEMETRYMODEMANAGER "dc_pustelemetrymodemanager"
+
+void DC_PUSTelemetryModeManager_register(void);
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                            class and struct
+//
+///////////////////////////////////////////////////////////////////////////////
+
+struct DC_PUSTelemetryModeManager {
+    TelemetryModeManager parent;
+
+    DC_PUSDataReportingPacket **list;    
+    unsigned int listLength;   
+    unsigned int iterationCounter;   
+    unsigned int counter;             // number of items in list
+};
+
+
+struct DC_PUSTelemetryModeManagerClass {
+    TelemetryModeManagerClass parent_class;
+};
+
+
+#define DC_PUSTELEMETRYMODEMANAGER_GET_CLASS(obj) \
+        OBJECT_GET_CLASS(DC_PUSTelemetryModeManagerClass, \
+                                          obj, TYPE_DC_PUSTELEMETRYMODEMANAGER)
+
+#define DC_PUSTELEMETRYMODEMANAGER_CLASS(klass) \
+        OBJECT_CLASS_CHECK(DC_PUSTelemetryModeManagerClass, \
+                                        klass, TYPE_DC_PUSTELEMETRYMODEMANAGER)
+
+#define DC_PUSTELEMETRYMODEMANAGER(obj) \
+        OBJECT_CHECK(DC_PUSTelemetryModeManager, \
+                                          obj, TYPE_DC_PUSTELEMETRYMODEMANAGER)
+
+
+DC_PUSTelemetryModeManager* DC_PUSTelemetryModeManager_new(void);
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                 non-virtual member method(s) declaration
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Load a telemetry packet in the set of packets managed by this
+ * mode manager. If the set is already full, then an event of 
+ * type EVT_ILLEGAL_MM is generated. Before calling this method, clients 
+ * should call method
+ * <code>isFull</code> to check whether there is space available.
+ * @param pItem the item to be loaded
+ */
+void DC_PUSTelemetryModeManager_loadPacket
+(
+    DC_PUSTelemetryModeManager *This, 
+    DC_PUSDataReportingPacket *pItem
+);
+
+/**
+ * Unload the PUS telemetry packet identified by the argument SID.
+ * If none of the currently loaded packets matches the given DC_PUSTelemetryModeManager_SID(or
+ * if the set is currently empty), 
+ * no action is taken other than generating an event report of type
+ * EVT_SID_NOT_FOUND. Method <code>isSIDLoaded</code> can be used 
+ * to check whether a packet with a certain SID is currently loaded.
+ * @param sid the SID of the packet to be unloaded
+ */
+void DC_PUSTelemetryModeManager_unloadPacket
+(
+    DC_PUSTelemetryModeManager *This, 
+    TD_SID sid
+);
+
+/**
+ * Check whether the mode manager is full. 
+ * @see #loadPacket
+ * @return true if no packets are currently loaded
+ */
+bool DC_PUSTelemetryModeManager_isFull
+(
+    const DC_PUSTelemetryModeManager *This
+);
+
+/**
+ * Return the number of packets that are currently loaded.
+ * @return number of packets currently loaded
+ */
+unsigned int DC_PUSTelemetryModeManager_getNumberOfPackets
+(
+    const DC_PUSTelemetryModeManager *This
+);
+
+/**
+ * Check whether the packet with the argument SID is currently loaded.
+ * @see #unloadPacket
+ * @param sid the SID 
+ * @return true if a packet with the argument SID is currently loaded
+ */
+bool DC_PUSTelemetryModeManager_isSIDLoaded
+(
+    const DC_PUSTelemetryModeManager *This, 
+    TD_SID sid
+);
+
+/**
+ * Set the maximum number of packets that can be loaded. 
+ * This is an initialization method. It should only be called once.
+ * Only positive values for the maximum number of packets are legal.
+ * @param n the maximum number of packets
+ */
+void DC_PUSTelemetryModeManager_setMaxNumberOfPackets
+(
+    DC_PUSTelemetryModeManager *This, 
+    unsigned int n
+);
+
+/**
+ * Return the maximum number of packets that can be loaded.
+ * @return maximum number of packets that can be loaded
+ */
+unsigned int DC_PUSTelemetryModeManager_getMaxNumberOfPackets
+(
+    const DC_PUSTelemetryModeManager *This
+);
+
+
+#endif
