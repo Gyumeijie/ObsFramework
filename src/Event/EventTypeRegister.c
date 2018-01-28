2309af37289bba9b54b35e66bf91533f870e52c9
Gyumeijie <Gyumeijie@gmail.com>
2018-01-28 13:33:16 +0800

    add DC_PUSEventRepository.

--- src/Event/EventTypeRegister.c
+++ src/Event/EventTypeRegister.c
@@ -1,9 +1,11 @@
 #include "EventTypeRegister.h"
 #include "DC_EventRepository.h"
 #include "DC_Event.h"
+#include "DC_PUSEventRepository.h"
 
 void event_type_register()
 {
    DC_Event_register();
    DC_EventRepository_register();
+   DC_PUSEventRepository_register();
 }
