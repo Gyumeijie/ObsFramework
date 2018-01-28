db33b8c0f2373c7577c778f99879c587c23180d4
Gyumeijie <Gyumeijie@gmail.com>
2018-01-28 17:54:49 +0800

    add CC_PunctualActionManager.

---
+++ src/Base/CC_PunctualActionManager.h
@@ -0,0 +1,116 @@
+//
+// Copyright 2004 P&P Software GmbH - All Rights Reserved
+//
+// CC_PunctualActionManager.h
+//
+// Version	1.0
+// Date		18.09.03
+// Author	A. Pasetti(P&P Software)
+
+
+#ifndef CC_PUNCTUALACTIONMANAGER_H
+#define CC_PUNCTUALACTIONMANAGER_H
+
+
+#include "../GeneralInclude/ForwardDeclarations.h"
+#include "../GeneralInclude/BasicTypes.h"
+#include "../Base/CC_RootObject.h"
+#include "PunctualActionModeManager.h"
+#include "../Qom/object.h"
+
+
+/*
+ * This class encapsulates a punctual action manager.
+ * The punctual action manager is responsible for controlling the execution
+ * of a list of punctual actions.
+ * The punctual actions are encapsulated in instances of <code>PunctualAction</code>
+ * components.
+ * The punctual actions to be executed are provided by a <i>punctual action
+ * mode manager</i> that is loaded as a plug-in component in the punctual
+ * action manager.
+ * <p>
+ * When the punctual action manager is activated, it asks its mode manager
+ * for the list of punctual actions to be executed and executes them in
+ * sequence.
+ * <p>
+ * @see PunctualAction
+ * @see PunctualActionModeManager
+ * @author Alessandro Pasetti(P&P Software GmbH)
+ * @version 1.0
+ * @ingroup FunctionalityManagers
+ */
+#define TYPE_CC_PUNCTUALACTIONMANAGER "cc_punctualactionmanager"
+
+void CC_PunctualActionManager_register(void);
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                            class and struct
+//
+///////////////////////////////////////////////////////////////////////////////
+
+struct CC_PunctualActionManager {
+    CC_RootObject parent;
+
+    PunctualActionModeManager *pModeManager;
+};
+
+
+struct CC_PunctualActionManagerClass {
+    CC_RootObjectClass parent_class;
+};
+
+
+#define CC_PUNCTUALACTIONMANAGER_GET_CLASS(obj) \
+        OBJECT_GET_CLASS(CC_PunctualActionManagerClass, \
+                                               obj, TYPE_CC_PUNCTUALACTIONMANAGER)
+
+#define CC_PUNCTUALACTIONMANAGER_CLASS(klass) \
+        OBJECT_CLASS_CHECK(CC_PunctualActionManagerClass, \
+                                             klass, TYPE_CC_PUNCTUALACTIONMANAGER)
+
+#define CC_PUNCTUALACTIONMANAGER(obj) \
+        OBJECT_CHECK(CC_PunctualActionManager, obj, TYPE_CC_PUNCTUALACTIONMANAGER)
+
+
+CC_PunctualActionManager* CC_PunctualActionManager_new(void);
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                 non-virtual member method(s) declaration
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Load the punctual action mode manager.
+ * @param pModeManager the telemetry mode manager
+ */
+void CC_PunctualActionManager_setPunctualActionModeManager
+(
+    CC_PunctualActionManager *This, 
+    PunctualActionModeManager *pModeManager
+);
+
+/**
+ * Get the punctual action mode manager.
+ * @return the punctual action mode manager
+ */
+PunctualActionModeManager* CC_PunctualActionManager_getPunctualActionModeManager
+(
+    const CC_PunctualActionManager *This
+);
+
+/**
+ * Execute the punctual actions supplied by the mode manager.
+ * A pseudocode implementation for this method is as follows: <PRE>
+ *      CC_PunctualActionManager_for(all pItem items provided by the puntual action mode manager)
+ *          pItem->CC_PunctualActionManager_execut e(CC_PunctualActionManager *This); </PRE>
+ * Note that the outcome of the punctual CC_PunctualActionManager_action(i.e. the return
+ * value of the <code>execute</code> method) is disregarded.
+ */
+void CC_PunctualActionManager_activate(CC_PunctualActionManager *This);
+
+
+#endif
---
+++ src/RegressionTest/TestCasePunctualActionManager_1.c
@@ -0,0 +1,165 @@
+//
+// Copyright 2004 P&P Software GmbH - All Rights Reserved
+//
+// TestCasePunctualActionManager_1.c
+//
+// Version	1.0
+// Date		30.09.03
+// Author	R. Totaro
+
+#include "../GeneralInclude/CompilerSwitches.h"
+#include "../GeneralInclude/ClassId.h"
+#include "../Base/DC_DummyPunctualAction.h"
+#include "../Base/DC_SimplePunctualActionModeManager.h"
+#include "../Base/CC_PunctualActionManager.h"
+#include "TestCasePunctualActionManager_1.h"
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                    non-pure virtual  method(s) definition
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Execute the test case. See class comment for details.
+ */
+static void runTestCase(void *obj)
+{
+    const unsigned int numberOfActions=4;
+
+    CC_PunctualActionManager *pPAM = CC_PunctualActionManager_new();
+    CC_RootObjectClass *cc_roc = CC_ROOTOBJECT_GET_CLASS(pPAM);
+
+    DC_SimplePunctualActionModeManager *pPAMM;
+    PunctualActionModeManagerClass *pammc;
+    pPAMM = DC_SimplePunctualActionModeManager_new();
+    ModeManagerClass *mmc = MODEMANAGER_GET_CLASS(pPAMM);
+
+    // Configure the mode manager and the add a newly created
+    // punctual actions to its only list
+    PunctualActionListModeManager_setListLength((void*)pPAMM, 0, numberOfActions);
+
+    for (unsigned int i=0; i<numberOfActions; i++) {
+        PunctualActionListModeManager_setListItem((void*)pPAMM, 0, i, 
+                                  (PunctualAction*)DC_DummyPunctualAction_new());
+    }
+
+    mmc->reset(pPAMM);
+
+	// Verify the correctness of the class id
+	if (CC_RootObject_getClassId((CC_RootObject*)pPAM) != ID_PUNCTUALACTIONMANAGER) 
+    {
+		TestCase_setTestResult((TestCase*)obj, TEST_FAILURE,
+                                "Wrong class id");
+		return;
+	}
+
+    // A newly created action manager shall not be configured
+    if (cc_roc->isObjectConfigured(pPAM)) 
+    {
+		TestCase_setTestResult((TestCase*)obj, TEST_FAILURE,
+                                "Wrong configuration status at creation");
+		return;
+    }
+
+    // The mode manager pointer shall be null
+    if (CC_PunctualActionManager_getPunctualActionModeManager(pPAM))
+    {
+		TestCase_setTestResult((TestCase*)obj, TEST_FAILURE,
+                               "Non NULL mode manager pointer at creation");
+		return;
+    }
+
+    // Now we set the mode manager pointer and we verify that it is properly
+    // stored and that the object is configured
+    CC_PunctualActionManager_setPunctualActionModeManager(pPAM, (void*)pPAMM);
+    if (CC_PunctualActionManager_getPunctualActionModeManager(pPAM) != (void*)pPAMM) 
+    {
+		TestCase_setTestResult((TestCase*)obj, TEST_FAILURE,
+                               "setPunctualActionModeManager() failed");
+		return;
+    }
+
+    if (!cc_roc->isObjectConfigured(pPAM)) 
+    {
+		TestCase_setTestResult((TestCase*)obj, TEST_FAILURE,
+                                "Wrong configuration status");
+		return;
+    }
+
+    // The activate() method is invoked twice and it is verified that
+    // the punctual actions' activation counters reflect this fact
+    CC_PunctualActionManager_activate(pPAM);
+    CC_PunctualActionManager_activate(pPAM);
+
+    pammc = PUNCTUALACTIONMODEMANAGER_GET_CLASS(pPAMM);
+    for (pammc->first(pPAMM);
+             pammc->isIterationFinished(pPAMM);
+                  pammc->next(pPAMM)) 
+    {
+        DC_DummyPunctualAction *pDPA;
+
+        pDPA = (DC_DummyPunctualAction*)pammc->getIterationPunctualAction(pPAMM);
+        if (DC_DummyPunctualAction_getActivationCounter(pDPA) != 2)
+        {
+            TestCase_setTestResult((TestCase*)obj, TEST_FAILURE, 
+                                    "activate() failed");
+            return;
+        }
+    }
+
+    TestCase_setTestResult((TestCase*)obj, TEST_SUCCESS, "Test Successful");
+	return;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                   object constructor and destructor
+//
+///////////////////////////////////////////////////////////////////////////////
+
+static void instance_init(Object *obj){}
+
+TestCasePunctualActionManager_1* TestCasePunctualActionManager_1_new(void)
+{
+    Object *obj = object_new(TYPE_TESTCASEPUNCTUALACTIONMANAGER_1);
+
+    TESTCASEGENERICSETUP_GET_CLASS(obj)->post_init((TestCaseGenericSetUp*)obj,
+                                                ID_PUNCTUALACTIONMANAGER*10+1,
+                                           "TestCasePunctualActionManager_1"); 
+
+    return (TestCasePunctualActionManager_1*)obj;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//                   binding and type registration
+//
+///////////////////////////////////////////////////////////////////////////////
+
+static void class_init(ObjectClass *oc, void *data)
+{
+    TestCaseClass *tcc = TESTCASE_CLASS(oc);
+    tcc->runTestCase = runTestCase;
+}
+
+static const TypeInfo type_info = {
+    .name = TYPE_TESTCASEPUNCTUALACTIONMANAGER_1,
+    .parent = TYPE_TESTCASEGENERICSETUP,
+    .instance_size = sizeof(TestCasePunctualActionManager_1),
+    .abstract = false,
+    .class_size = sizeof(TestCasePunctualActionManager_1Class),
+    .instance_init = instance_init,
+    .class_init = class_init,
+};
+
+void TestCasePunctualActionManager_1_register(void)
+{
+    type_register_static(&type_info);
+}
