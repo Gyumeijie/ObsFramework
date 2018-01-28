54b07e247a98f62f544f8b2d92505e9d38cf7d6c
Gyumeijie <Gyumeijie@gmail.com>
2018-01-28 14:33:05 +0800

    feat: add PUSFunctionManagement

--- src/Telecommand/TelecommandTypeRegister.c
+++ src/Telecommand/TelecommandTypeRegister.c
@@ -19,6 +19,8 @@
 #include "DC_BasicPUSTcLoader.h"
 #include "DC_PUSClearDataReporting.h"
 #include "DC_PUSControlDataReporting.h"
+#include "DC_PUSDefineDataReporting.h"
+#include "PUSFunctionManagement.h"
 
 void telecommand_type_register(void)
 {
@@ -40,6 +42,8 @@ void telecommand_type_register(void)
    DC_TestPUSTelecommand_register();
    DC_PUSClearDataReporting_register();
    DC_PUSControlDataReporting_register();
+   DC_PUSDefineDataReporting_register();
+   PUSFunctionManagement_register();
 
    TelecommandLoader_register();
    DC_DummyTelecommandLoader_register();
